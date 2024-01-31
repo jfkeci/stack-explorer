19c861846c0449df6a56248b1f3cfbfd490a0640
jfkeci <jakov-filip.sabolicek@almamedia.com>
2024-01-31 13:22:11 +0100

    chore: organize imports

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { envConfig } from './utils/config/env.config';
-import { TypeOrmConfigService } from './utils/module/typeorm-config/service/typeorm-config.service';
 import { TypeormConfigModule } from './utils/module/typeorm-config/typeorm-config.module';
+import { TypeOrmConfigService } from './utils/module/typeorm-config/service/typeorm-config.service';
 
 @Module({
   imports: [
